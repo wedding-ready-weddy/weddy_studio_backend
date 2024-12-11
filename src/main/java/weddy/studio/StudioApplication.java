b036cea1c0691a337020036c3aa7d085880e1c1d
Haram0111 <5gkfka5@naver.com>
2024-12-11 15:50:25 +0900

    first commit

---
+++ src/main/java/weddy/studio/StudioApplication.java
@@ -0,0 +1,13 @@
+package weddy.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/weddy/studio/StudioApplicationTests.java
@@ -0,0 +1,13 @@
+package weddy.studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
